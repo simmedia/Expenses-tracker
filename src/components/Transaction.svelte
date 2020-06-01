815f4b0a59cbc22fb964483bcd391aadcf7a1508
simmedia <simadurlan92@gmail.com>
2020-06-01 17:02:12 +0200

    Transition added

--- src/components/Transaction.svelte
+++ src/components/Transaction.svelte
@@ -1,9 +1,11 @@
 <script>
-export let transaction
-export let removeTransaction
+  import { slide } from "svelte/transition";
+  export let transaction;
+  export let removeTransaction;
 </script>
 
 <div
+  transition:slide
   class="notification is-light {transaction.value > 0 ? 'is-success' : 'is-danger'}">
   {transaction.value}
   <button class="delete" on:click={() => removeTransaction(transaction._id)} />
